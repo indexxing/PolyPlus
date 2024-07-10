35fe13d6a2c7d588593672133fe82ad12c792dc1
Index <xv.media.address@gmail.com>
2024-07-10 07:40:37 -0500

    minor: update registerTooltips.js to register dropdowns

--- resources/registerTooltips.js
+++ resources/registerTooltips.js
@@ -1,2 +1,5 @@
 var tooltips = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-var list = [...tooltips].map((tool) => new bootstrap.Tooltip(tool));
+var tooltip_list = [...tooltips].map((tool) => new bootstrap.Tooltip(tool));
+
+var dropdowns = document.querySelectorAll('[data-bs-toggle="dropdown"]');
+var dropdown_list = [...dropdowns].map((dropdown) => new bootstrap.Dropdown(dropdown));
